d64a155c2c7bbf2fb175bc34237fa99e9c367a05
Stelios <steliostamsios05@gmail.com>
2026-06-15 15:19:06 +0300

    Update clock.js

--- clock.js
+++ clock.js
@@ -1,9 +1,9 @@
 setInterval(() => {
 
-  let hours = document.getElementById('hours');
-  let minutes = document.getElementById('minutes');
+  let hours    = document.getElementById('hours');
+  let minutes  = document.getElementById('minutes');
   let secondes = document.getElementById('seconds');
-  let ampm = document.getElementById('ampm');
+  let ampm     = document.getElementById('ampm');
 
   let hh = document.getElementById('hh');
   let mm = document.getElementById('mm');
@@ -14,16 +14,14 @@ setInterval(() => {
   let s = new Date().getSeconds();
   let ap = h >= 12 ? 'PM' : 'AM';
 
-  if (h > 12) {
-    h = h - 12;
-  }
+  if (h > 12) {h = h - 12;}
 
   h = h < 10 ? '0' + h : h;
   m = m < 10 ? '0' + m : m;
   s = s < 10 ? '0' + s : s;
 
-  hours.innerHTML = h + ' <br>Hours';
-  minutes.innerHTML = m + ' <br>Minutes';
+  hours.innerHTML = h +    ' <br>Hours';
+  minutes.innerHTML = m +  ' <br>Minutes';
   secondes.innerHTML = s + ' <br>Seconds';
   ampm.innerHTML = ap;
 
@@ -31,4 +29,5 @@ setInterval(() => {
   mm.style.strokeDashoffset = 440 - (440 * m) / 60;
   ss.style.strokeDashoffset = 440 - (440 * s) / 60;
   
-}, 1000);
+}, 
+1000);
